23a2acf06890b0f7d333e40609defe2f9e4443fc
Pandey-Shailesh <105715056+Pandey-Shailesh@users.noreply.github.com>
2022-05-23 10:04:52 +0530

    functions and its prototypes

--- tut15.cpp
+++ tut15.cpp
@@ -1,11 +1,11 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-//Function prototyping
-//    syntex:-  type function_name(arguments);
+// Function prototyping
+//     syntex:-  type function_name(arguments);
 
-int sum(int a,int b);
+int sum(int a, int b);
 
 // int sum(int a,int b);---------->acceptable
 // int sum(int a,b);----------------->not acceptable
@@ -16,30 +16,31 @@ int main()
 
 {
 
-//******************************Functions in C++********************************
-// Functions are the main part of top-down structured programming. 
-// We break the code into small pieces and make functions of that code. 
-// Functions help us to reuse the code easily.
-g();
-int num1; int num2;
-cout<<"enter the first number"<<endl;
-cin>>num1;
-cout<<"enter the second number"<<endl;
-cin>>num2;
-cout<<"the sum is "<<sum(num1,num2);
+    //******************************Functions in C++********************************
+    // Functions are the main part of top-down structured programming.
+    // We break the code into small pieces and make functions of that code.
+    // Functions help us to reuse the code easily.
+    g();
+    int num1;
+    int num2;
+    cout << "enter the first number" << endl;
+    cin >> num1;
+    cout << "enter the second number" << endl;
+    cin >> num2;
+    cout << "the sum is " << sum(num1, num2);
 
-//num1 and num2 are actual parameters.
+    // num1 and num2 are actual parameters.
 
     return 0;
-
 }
 
-int sum(int a,int b)
+int sum(int a, int b)
 // Formal parameter:- a and b will taking the value from (Actual parameter) num1 and num2.
 {
-int c=a+b;
-return c;  
+    int c = a + b;
+    return c;
 }
-void g(){
-    cout<<"Hello, Good Morning"<<endl;
+void g()
+{
+    cout << "Hello, Good Morning" << endl;
 }
