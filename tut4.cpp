6c0691c810bcbb055d0f08017e5c8fbba5b7b235
Pandey-Shailesh <105715056+Pandey-Shailesh@users.noreply.github.com>
2022-05-23 09:51:38 +0530

    global  declaration

--- tut4.cpp
+++ tut4.cpp
@@ -1,28 +1,29 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
-int glo= 9;
- int sum(){
-     int a;
-     cout<<glo;
-     return 0;
-  }
+int glo = 9;
+int sum()
+{
+    int a;
+    cout << glo;
+    return 0;
+}
 
 int main()
 
 {
 
+    int glo = 15;
+    glo = 17;
+    int a = 2, b = 4;
+    float pi = 3.14;
+    char you = 's';
+    bool is = false;
 
-    int glo =15;
-    glo=17;
-    int a=2, b=4;
-    float pi=3.14;
-    char you='s';
-    bool is=false;
-    
-    cout<<"\nhear the value of a is "<<a<<" and \nthe value of b is "<<b;
-    cout<<"\nthe value of pi is "<<pi<<"\nthe value of you is "<<you ;
+    cout << "\nhear the value of a is " << a << " and \nthe value of b is " << b;
+    cout << "\nthe value of pi is " << pi << "\nthe value of you is " << you;
     sum();
-    cout<<"\n the value of glo is "<<glo<<"\n"<<is;
+    cout << "\n the value of glo is " << glo << "\n"
+         << is;
     return 0;
 }
