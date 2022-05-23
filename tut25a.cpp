3ea650c7ee0818841d3062671c43e856bf0dcf60
Pandey-Shailesh <105715056+Pandey-Shailesh@users.noreply.github.com>
2022-05-23 10:16:08 +0530

    array of objects and passing objects as function arguments

--- tut25a.cpp
+++ tut25a.cpp
@@ -1,5 +1,4 @@
- //*********Array of Objects in C++*****
-
+//*********Array of Objects in C++*****
 
 /*An array of Objects in C++
 
@@ -7,38 +6,37 @@
 2.An array of objects consists of class objects as its elements.
 3.If the array consists of class objects it is called an array of objects. */
 
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 class Employee
 {
- int Id;
- int salary;
- public:
- void setId(void)
-{
-  salary==112;
-  cout<<"Enter the employe id "<<endl;
-  cin>>Id;
-}
+    int Id;
+    int salary;
 
-void getId(void)
-{
-    cout<<"the Id of the employe is  "<<Id<<endl;
-}
+public:
+    void setId(void)
+    {
+        salary == 112;
+        cout << "Enter the employe id " << endl;
+        cin >> Id;
+    }
 
+    void getId(void)
+    {
+        cout << "the Id of the employe is  " << Id << endl;
+    }
 };
 
 int main()
 {
-Employee fib[4];
+    Employee fib[4];
 
-for (int i = 0; i < 4; i++)
-{
-    fib->setId();
-    fib->getId();
-
-}
+    for (int i = 0; i < 4; i++)
+    {
+        fib->setId();
+        fib->getId();
+    }
     return 0;
 }
