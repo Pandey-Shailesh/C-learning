6b6d46b4f96d2f485ff68eb2e250db6bcc1bd90c
Pandey-Shailesh <105715056+Pandey-Shailesh@users.noreply.github.com>
2022-05-23 10:01:10 +0530

    break and continue statements

--- tut11.cpp
+++ tut11.cpp
@@ -1,33 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 // Berak and continue statement
 int main()
 {
 
-cout<<"break Statement"<<endl;
+    cout << "break Statement" << endl;
 
-for (int i=0;i<50;i++)
-{
-     cout<<i<<endl;
-    if(i==3)
+    for (int i = 0; i < 50; i++)
     {
-        break;
+        cout << i << endl;
+        if (i == 3)
+        {
+            break;
+        }
     }
-}
 
+    cout << "contineue statement" << endl;
 
-   cout<<"contineue statement"<<endl;
-  
-   for (int a=0;a<50;a++)
-   {
-     
-       if(a==5)
-       {
-           continue;
-       }
-         cout<<a<<endl;
-   }
-   
+    for (int a = 0; a < 50; a++)
+    {
+
+        if (a == 5)
+        {
+            continue;
+        }
+        cout << a << endl;
+    }
+
     return 0;
 }
