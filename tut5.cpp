9a2291a8c8967d7980290da09cf32506c58d924f
Pandey-Shailesh <105715056+Pandey-Shailesh@users.noreply.github.com>
2022-05-23 09:52:53 +0530

    basic input and output program

--- tut5.cpp
+++ tut5.cpp
@@ -1,20 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-
-
-
 
 int main()
 {
-  int num1,num2;
-  cout<<"enter the value of num1";    /*  << this is an insertion operator */
-  cin>>num1;                         /*  >> this is exteaction operator*/
+  int num1, num2;
+  cout << "enter the value of num1"; /*  << this is an insertion operator */
+  cin >> num1;                       /*  >> this is exteaction operator*/
 
+  cout << "enter the value of num2";
+  cin >> num2;
 
-  cout<<"enter the value of num2";  
-  cin>>num2;  
-
-cout<<"the sum is "<< num1+num2;
-    return 0;
+  cout << "the sum is " << num1 + num2;
+  return 0;
 }
